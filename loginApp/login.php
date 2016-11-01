84c709caa5be55af8d08d399f9f78c01e50571c5
Adnan <adnann.rasheed@gmail.com>
2016-11-02 01:46:38 +0500

    Update

--- loginApp/login.php
+++ loginApp/login.php
@@ -2,30 +2,51 @@
 include 'header.html';
 include 'functions.php';
 session_start ();
-function sign_in()
+
+/**
+ * **************************************************************
+ */
+function get_user($connection)
 {
-	$username = $_POST ['user_id'];
-	$pwd = $_POST ['pwd'];
-	$dbConnection = get_db_connection ();
-	$does_user_exist = "select * from users where user_id = '$username' and pwd ='$pwd'";
-	$result_existance = mysqli_query ( $dbConnection, $does_user_exist );
-	// $result = mysqli_fetch_array($result);
-	$rows = mysqli_num_rows ( $result_existance );
-	if ($rows == 1)
+	$username = $_POST ['user_name'];
+	$pwd = $_POST ['password'];
+	$user_select_query = "select * from userinfo where user_name = '$username' and password ='$pwd'";
+	$user = mysqli_query ( $connection, $user_select_query );
+	if (is_object ( $user ))
 	{
-		// set a session variable to know if user has logged in
-		$_SESSION ['username'] = $username;
-		$get_the_user_info = "select first_name,last_name from users where user_id = '$username'";
-		$result_info = mysqli_query ( $dbConnection, $get_the_user_info );
-		$array = mysqli_fetch_assoc ( $get_the_user_info );
-		$_SESSION ['first_name'] = $array ['first_name'];
-		$_SESSION ['last_name'] = $array ['last_name'];
-		close_the_connection ( $dbConnection );
-		// launch the landing page
-		header ( "Location: landingPage.php" );
+		// making row to check is there a row
+		$num_row = mysqli_num_rows ( $user );
+		if ($num_row == 1)
+		{
+			return $user;
+		}
+	}
+	return null;
+}
+
+/**
+ * **************************************************************
+ */
+function set_login_session($array)
+{
+	$_SESSION ['username'] = $array ['user_name'];
+	$_SESSION ['p_img'] = $array ['image'];
+	// taking the user to the landingpage
+	header ( "Location: landingPage.php" );
+}
+/**
+ * **************************************************************
+ */
+function login_user()
+{
+	$dbConnection = get_db_connection ();
+	$user = get_user ( $dbConnection );
+	if ($user !== null)
+	{
+		$array = mysqli_fetch_assoc ( $user );
+		set_login_session ( $array );
 	} else
-		close_the_connection ($dbConnection);
-	return false;
+		return false;
 }
 
 ?>
@@ -34,27 +55,25 @@ function sign_in()
 	<form class="form-horizontal" role="form" action="login.php"
 		method="post">
 		<div class="from-group">
-			<label for="user_id">Username</label> <input type="text"
-				class="form-control" name="user_id">
+			<label for="user_name">Username</label> <input type="text"
+				class="form-control" name="user_name">
 		</div>
 		<div class="from-group">
-			<label for="pwd">Password</label> <input type="Password"
-				class="form-control" name="pwd">
+			<label for="password">Password</label> <input type="Password"
+				class="form-control" name="password">
 		</div>
 		<br>
+		
 		<?php
 		if (! empty ( $_POST ))
 		{
-			$do_sign_in = sign_in ();
-			if (! $do_sign_in)
+			$success = login_user ();
+			if (! $success)
 			{
-				echo '<div class="alert alert-danger" role="alert">
-			Invalid id or password!
-            </div>';
+				echo '<div class="alert alert-warning">Username and password is incorrect</div>';
 			}
 		}
 		?>
-		
 		
 		<div class="from-group">
 			<input class="btn btn-default" type="Submit" value="Log in">
@@ -64,4 +83,5 @@ function sign_in()
 </div>
 
 <?php
-include 'footer.html';?>
+include 'footer.html';
+?>
